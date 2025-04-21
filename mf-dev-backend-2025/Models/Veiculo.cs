6989b3240138fe0dcc5d4433372ab1e4cd927340
fer-puc-minas <fernando.gavioli@sga.pucminas.br>
2025-04-21 15:02:38 -0300

    crude_criado

--- mf-dev-backend-2025/Models/Veiculo.cs
+++ mf-dev-backend-2025/Models/Veiculo.cs
@@ -13,8 +13,10 @@ namespace mf_dev_backend_2025.Models
         [Required(ErrorMessage = "Obrigatório informar a placa!")]
         public string Placa { get; set; }
         [Required(ErrorMessage = "Obrigatório informar o ano de fabricação!")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
         [Required(ErrorMessage = "Obrigatório informar o ano do modelo!")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
     }
 }
