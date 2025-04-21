70497ab3e178e5bd2ab3cc1b569b4619b7862304
fer-puc-minas <fernando.gavioli@sga.pucminas.br>
2025-04-21 18:12:48 -0300

    adicao_login_senha

---
+++ mf-dev-backend-2025/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2025.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar a senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        public Perfil Perfil { get; set; }
+
+    }
+
+    public enum Perfil
+    {
+        Admin,
+        User,
+    }
+}
